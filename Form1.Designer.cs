aea37a72780bce73225c308ca769f8b6e83c66aa
yangnyeom06 <lyujin1204@gmail.com>
2026-03-12 16:08:41 +0900

    버튼의 기본 도망 기능

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(302, 331);
+            button1.Name = "button1";
+            button1.Size = new Size(161, 53);
+            button1.TabIndex = 0;
+            button1.Text = "나를 잡아봐";
+            button1.MouseEnter += button1_MouseEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,20 @@ namespace Winform2
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            int maxX = this.ClientSize.Width - button1.Width;
+            int maxY = this.ClientSize.Height - button1.Height;
+            int nextX = rd.Next(0, maxX); int nextY = rd.Next(0, maxY);
+            button1.Location = new Point(nextX, nextY);
+            this.Text = $"버튼위치: ({nextX}, {nextY})";
         }
     }
 }
